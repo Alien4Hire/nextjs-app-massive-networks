5f213fd5901922100cce55762b628a8f580ccbbe
Alien Hire <jaymaynard84@gmail.com>
2023-11-02 01:49:06 +0500

    poc: link stencil components with next

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+// pages/_app.js or pages/_app.tsx
+import { useEffect } from 'react';
+
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    import('massive-networks-shared-web-components/loader').then((module) => {
+      module.defineCustomElements(window);
+    });
+  }, []);
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import * as something from 'massive-networks-shared-web-components'
-console.log(something)
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    {/* <mass-google-map
-        center={lat:37.7849, lng: -122.4294}
+
+const IndexPage = () => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+       {/* @ts-ignore */}
+    <mass-google-map
+        center={{lat:37.7849, lng: -122.4294}}
         coordinates={[
           { lat: 37.7749, lng: -124.4194 },
           { lat: 37.7849, lng: -122.4294 } 
         ]}
-    /> */}
+    />
     {/* @ts-ignore */}
-    {/* <mass-button>Button</mass-button> */}
+    <mass-button>Button</mass-button>
   </Layout>
-)
+  )
+
+  }
 
 export default IndexPage
