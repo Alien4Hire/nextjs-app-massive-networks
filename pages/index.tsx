f9d02a04906fa609fb6c67c2266b8b47ed3efa1e
Alien Hire <jaymaynard84@gmail.com>
2023-11-18 02:45:02 +0500

    left-menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 // @ts-nocheck
 import Layout from "../components/Layout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const IndexPage = () => {
   const [searchRadius, setSearchRadius] = useState("");
   const [searchAddress, setSearchAddress] = useState("");
+  const [displayLeftMenu, setDisplayLeftMenu] = useState(false);
   const [counter, setCounter] = useState(0);
   const [results, setResults] = useState([]);
+  const [savedSearches, setSavedSearches] = useState([]);
+
+  useEffect(() => {
+    loadSearchesFromLocalStorage();
+  }, []);
+
+  const loadSearchesFromLocalStorage = () => {
+    const savedSearches = JSON.parse(localStorage.getItem("searches")) || [];
+    setSavedSearches(savedSearches);
+  };
 
   const parseMarker = (input) => {
     let newObj = {};
@@ -25,6 +36,18 @@ const IndexPage = () => {
       : 0;
 
     return newObj;
+  };
+
+  const saveSearchToLocalStorage = (searchData) => {
+    const searches = JSON.parse(localStorage.getItem("searches")) || [];
+
+    searches.unshift(searchData);
+
+    if (searches.length > 100) {
+      searches.length = 100;
+    }
+
+    localStorage.setItem("searches", JSON.stringify(searches));
   };
 
   const search = async () => {
@@ -60,12 +83,17 @@ const IndexPage = () => {
       const coords = body.map((marker) => parseMarker(marker));
       populateData(coords);
       handleProgressBar(false, 0);
+      saveSearchToLocalStorage({
+        search: { searchAddress, searchRadius },
+        coords,
+      });
     } catch (error) {
       console.error("failed to fetch markers for the entered address");
     }
   };
 
-  const populateData = (coords) => {
+  const populateData = (coords, manualRadius = "") => {
+    console.log(manualRadius, "manualRadius");
     const mapComponent = document.querySelector("mass-google-map");
     mapComponent.center = {
       lat: coords[0]["lat"] || 39.9654502,
@@ -104,7 +132,7 @@ const IndexPage = () => {
         title: "Ethernet - Coax",
       },
     ];
-    mapComponent.zoom = parseInt(searchRadius) || 0;
+    mapComponent.zoom = parseInt(manualRadius || searchRadius) || 0;
   };
 
   const handleProgressBar = (isVisible, progress) => {
@@ -115,6 +143,87 @@ const IndexPage = () => {
   return (
     <Layout title="Massive Networks Lit Building Locator">
       <mass-progress-bar />
+      {displayLeftMenu && (
+        <mass-leftmenu>
+          <div className="flex flex-col items-center p-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 absolute top-4 left-4"
+              onClick={() => setDisplayLeftMenu(false)}
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+
+            <h1>Last Searches</h1>
+          </div>
+          <div className="flex flex-col items-center overflow-y-auto h-screen">
+            {savedSearches.map((item, index) => (
+              <div
+                className="max-w-sm mt-2 mb-2 p-6 bg-white border border-gray-200 rounded-lg shadow"
+                key={index.toString}
+              >
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                    />
+                  </svg>
+
+                  <div className="ml-3">
+                    <p className="mb-3 font-normal text-gray-700">
+                      {item.search.searchAddress}{" "}
+                      <span className="font-normal text-grey-400">
+                        (Radius: {item.search.searchRadius}km)
+                      </span>
+                    </p>
+                    <a
+                      href="#"
+                      onClick={() =>
+                        populateData(item.coords, item.search.searchRadius)
+                      }
+                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Search
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-4 h-4 ml-2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </mass-leftmenu>
+      )}
+
       <div className="flex flex-col min-h-screen">
         <div
           style={{ position: "relative", zIndex: 10, backgroundColor: "white" }}
@@ -181,8 +290,7 @@ const IndexPage = () => {
               style={{
                 marginLeft: "auto",
                 padding: "5px 2px",
-                width: "240px",
-                justifyContent: "space-around",
+                width: "310px",
                 display: "flex",
               }}
               className="flex space-x-2 ml-auto"
@@ -213,7 +321,21 @@ const IndexPage = () => {
                   setSearchRadius("");
                 }}
               >
-                Reset Map
+                Reset
+              </mass-button>
+              <mass-button
+                button-id="ResetId"
+                button-text="Reset Map"
+                disabled={false}
+                icon-position="none"
+                size="medium"
+                type="blue"
+                tone="light"
+                onClick={() => {
+                  setDisplayLeftMenu(true);
+                }}
+              >
+                History
               </mass-button>
             </div>
           </div>
